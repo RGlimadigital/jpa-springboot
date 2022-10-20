63ac88b33d6eb5955f40ebbbfabdf8e6c016f4fb
RGlimadigital <RgFullDev@gmail.com>
2022-10-20 12:38:28 -0300

    Criando a entidade de Pedidor

--- src/main/java/com/exercicio/course/repositories/UserRepository.java
+++ src/main/java/com/exercicio/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.exercicio.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.exercicio.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
