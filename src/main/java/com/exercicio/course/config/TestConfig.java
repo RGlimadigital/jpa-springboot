5cb3694979cae29de5b24d41cc58656df280eeae
RGlimadigital <RgFullDev@gmail.com>
2022-10-25 08:08:31 -0300

    Criando a entidade de Categorias

--- src/main/java/com/exercicio/course/config/TestConfig.java
+++ src/main/java/com/exercicio/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.exercicio.course.entities.Category;
 import com.exercicio.course.entities.Order;
 import com.exercicio.course.entities.User;
 import com.exercicio.course.entities.enums.OrderStatus;
+import com.exercicio.course.repositories.CategoryRepository;
 import com.exercicio.course.repositories.OrderRepository;
 import com.exercicio.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,16 +38,22 @@ public class TestConfig implements CommandLineRunner{
 		User u3 = new User(null, "Rodrigo Guedes", "rodrigoguedes@mail.com","+5562991735771", "1234567");
 		User u4 = new User(null, "Angela Frison", "angelaafrison@hotmail.com","+55629934167169", "1234567");
 		
+		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
+		
 		//Orders - data formato ISO8601
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.WAITTING_PAYMENT, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:53:07Z"),OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T19:53:07Z"),OrderStatus.SHIPPED, u1);
 		
-		
-		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
-		
-		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		//Instanciando as Categorias:
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 	} 
 
 }
