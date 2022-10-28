8d8adde98b3122f9df02e9fc96a37680528bf410
RGlimadigital <RgFullDev@gmail.com>
2022-10-28 09:50:20 -0300

    Criando as categorias e os produtos

--- src/main/java/com/exercicio/course/config/TestConfig.java
+++ src/main/java/com/exercicio/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.exercicio.course.entities.Category;
 import com.exercicio.course.entities.Order;
+import com.exercicio.course.entities.Product;
 import com.exercicio.course.entities.User;
 import com.exercicio.course.entities.enums.OrderStatus;
 import com.exercicio.course.repositories.CategoryRepository;
 import com.exercicio.course.repositories.OrderRepository;
+import com.exercicio.course.repositories.ProductRepository;
 import com.exercicio.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -53,6 +58,14 @@ public class TestConfig implements CommandLineRunner{
 		Category cat3 = new Category(null, "Computers");
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		//Instanciando os Produtos:
+		Product prod1 = new Product(null, "The Lord of the Rings", "Livro Capa dura edicao especial", 132.6, "");
+		Product prod2 = new Product(null, "Smart Tv LG", "Conexao WiFi , aplicativos e Som 5.1", 4999.9, "");
+		Product prod3 = new Product(null, "MacBook Pro", "16Gb de RAM , processador Intel i9", 19990.00, "");
+		Product prod4 = new Product(null, "PC Gamer", "Intel i9 , 16Gb de Ram e 1Tb de disco duro", 9999.9, "");
+		productRepository.saveAll(Arrays.asList(prod1, prod2, prod3, prod4));
+		
 		
 	} 
 
--- src/main/java/com/exercicio/course/entities/Category.java
+++ src/main/java/com/exercicio/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.exercicio.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 
 @Entity
@@ -19,6 +22,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -39,6 +46,10 @@ public class Category implements Serializable {
 
 	public String getName() {
 		return name;
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	public void setName(String name) {
@@ -66,9 +77,5 @@ public class Category implements Serializable {
 	public String toString() {
 		return "Category [id=" + id + ", name=" + name + "]";
 	}
-	
-	
-	
-	
 
 }
